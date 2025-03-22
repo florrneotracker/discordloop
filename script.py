bd6e842c04c713444affe885b3468ee10473e260
florrneotracker <florrballs69@gmail.com>
2025-03-22 18:05:27 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,21 @@
-# Remove while True loop
+import os
+import time
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Initialize WebDriver
-driver = webdriver.Chrome(options=options)
+# Set up Selenium Chrome options
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")  
+options.add_argument("--no-sandbox")  
+options.add_argument("--disable-dev-shm-usage")  
+options.add_argument("--disable-gpu")  
+
+# Initialize WebDriver with ChromeDriverManager
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+# Remove while True loop
 
 # Open webpage
 driver.get("https://guestydaysreal.pythonanywhere.com/super?pretty=true&judgement=true")
